2288faebce78874be15cecb7f094aa854fe07161
Joao Chaves <chavesjj@outlook.com>
2020-11-08 23:23:58 -0300

    admin cities states and seed

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,16 @@
 //= require active_admin/base
+$(document).ready(function() {
+  $("#park_state_id").change(function() {
+     getCitiesByState("id="+$("#park_state_id").val());
+   });
+ });
+
+ function getCitiesByState(id) {
+   $.getJSON("/admin/parks/cities_by_state", id, function(j) {
+     var options = '<option value="">SELECIONE A CIDADE</option>';
+     $.each(j.cty, function(i, item) {
+       options += '<option value="' + item.id + '">' + item.n + '</option>';
+     });
+     $("#park_city_id").html(options);
+   });
+ }
